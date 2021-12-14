05644752ff24f8e0bbdc936ef7941ab150ba2f31
hqics <paul.bruening@icloud.com>
2021-12-14 08:32:11 +0100

    Remove the need for fabric api

--- src/main/java/me/hqics/mods/ContainerMenuDupeFixMod.java
+++ src/main/java/me/hqics/mods/ContainerMenuDupeFixMod.java
@@ -4,10 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ContainerMenuDupeFixMod implements ModInitializer {
+public class ContainerMenuDupeFixMod {
     public static final Logger LOGGER = LogManager.getLogger("containermenudupefixmod");
-
-    @Override
-    public void onInitialize() {
-    }
 }
